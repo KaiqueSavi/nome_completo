3a3a41d50f4e219a50f42e1686a5c09c97bef4b2
Kaique Savi <kaiquesavi1@gmail.com>
2024-05-30 20:09:30 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('form').on('submit', function(e){
+
+        e.preventDefault();
+        
+        let tarefa = $('#tarefa-value').val();
+
+        let novaTarefa = $(`<li>${tarefa}</li>`); //Adiciona tarefa 
+
+        $(novaTarefa).appendTo('ul');
+
+        $('#tarefa-value').val('');
+
+    })
+    
+    $('li').click(function(){
+        $('li').toggleClass('concluido');
+    })
+
+    });
